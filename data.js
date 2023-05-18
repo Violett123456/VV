b1f6ef30d8b096258a53b56e4663cd9d0b68cde0
Violett123456 <133950494+Violett123456@users.noreply.github.com>
2023-05-19 01:21:23 +0300

    Upload

---
+++ data.js
@@ -0,0 +1,295 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--_",
+      "name": "Кухня-Гостиная_Панорама",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7622209973665592,
+          "pitch": 0.4793413523335026,
+          "rotation": 0,
+          "target": "1---_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---_",
+      "name": "Кухня-Гостиная-Прихожая_Панорама",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0006257334851582641,
+          "pitch": 0.3258810485437458,
+          "rotation": 0,
+          "target": "0--_"
+        },
+        {
+          "yaw": 2.7429011853400898,
+          "pitch": 0.3945956483180524,
+          "rotation": 0,
+          "target": "4--1-_"
+        },
+        {
+          "yaw": -2.181360205586145,
+          "pitch": 0.35416573604906176,
+          "rotation": 0,
+          "target": "2-_"
+        },
+        {
+          "yaw": 3.119513383639214,
+          "pitch": -0.1317823273377865,
+          "rotation": 0,
+          "target": "5--2-_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-_",
+      "name": "Прихожая_Панорама",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6711480979480902,
+          "pitch": 0.3280254224174932,
+          "rotation": 0,
+          "target": "3-_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-_",
+      "name": "Гардеробная_Панорама",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5215581773990827,
+          "pitch": 0.354995749812117,
+          "rotation": 0,
+          "target": "2-_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--1-_",
+      "name": "Санузел 1 Этажа_Панорама",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4186808305193495,
+          "pitch": 0.3104975190953212,
+          "rotation": 0,
+          "target": "1---_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--2-_",
+      "name": "Холл 2 Этажа_Панорама",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1426660737742598,
+          "pitch": 0.2699677516170862,
+          "rotation": 0,
+          "target": "6--2-_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--2-_",
+      "name": "Санузел 2 Этажа_Панорама",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6766318302804812,
+          "pitch": -0.04464744086375205,
+          "rotation": 0,
+          "target": "5--2-_"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
